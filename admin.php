fa177b70204309954adf96e22bf3c117c5bc7071
Koonri <92974237+Koonri@users.noreply.github.com>
2022-10-27 18:15:36 +0200

    pain

--- admin.php
+++ admin.php
@@ -1,8 +1,14 @@
 <?php 
 $db = new mysqli('localhost', 'root', '', 'ristorante');
 
-$q = "SELECT nazwa, cena, id FROM dania";
+$q = "SELECT nazwa, cena, times_ordered FROM dania";
 
+$result = $db->query($q);
+while($row = $result->fetch_assoc()) {
+    echo '<p>'.$row['nazwa']."<br>";
+    echo $row['cena'].'<br>';
+    echo $row['times_ordered'].'<br></p>';
+}
 
 
 
